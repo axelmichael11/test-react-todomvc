ac9d853ca58a03297f236b152ec5e0dc21319b4d
Michael Axelson <michaelaxelson@Michaels-MBP.PK5001Z>
2018-08-16 11:05:44 -0700

    added base npm packages, webpack.config file, html scaffold

---
+++ webpack.config.js
@@ -0,0 +1,101 @@
+'use strict'
+
+require('dotenv').config()
+const production = process.env.NODE_ENV || 'dev';
+const { DefinePlugin, EnvironmentPlugin } = require('webpack')
+const webpack = require('webpack')
+const HTMLPlugin = require('html-webpack-plugin')
+const CleanPlugin = require('clean-webpack-plugin')
+const UglifyPlugin = require('uglifyjs-webpack-plugin')
+const ExtractPlugin = require('extract-text-webpack-plugin')
+const CompressionPlugin = require('compression-webpack-plugin');
+const path = require('path');
+
+
+
+
+let plugins = [
+  new EnvironmentPlugin({NODE_ENV:'development', DEBUG:false}),
+  new ExtractPlugin('bundle-[hash].css'),
+  new HTMLPlugin({ template: `${__dirname}/index.html` }),
+  new DefinePlugin({
+    'process.env': {
+      __PORT__: JSON.stringify(process.env.PORT),
+    }
+  }),
+]
+
+
+console.log('this is our production thing!!!!', process.env.NODE_ENV);
+
+
+module.exports = {
+  plugins,
+  entry: `${__dirname}/src/app.js`,
+  devServer: {
+    historyApiFallback: true,
+  },
+  devtool: production ? undefined : 'cheap-module-eval-source-map',
+  output: {
+    path: `${__dirname}/build`,
+    filename: 'bundle-[hash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.es6$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractPlugin.extract(['css-loader', 'sass-loader']),
+      },
+      {
+        test: /\.icon.svg$/,
+        loader: 'raw-loader',
+      },
+      {
+        test: /\.(woff|woff2|ttf|eot).*/,
+        exclude: /\.icon.svg/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              name: 'font/[name].[hash].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(pdf|jpg|jpeg|gif|png|tiff|svg)$/,
+        exclude: /\.icon.svg$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 60000,
+              name: '[name].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(mp3|aac|aiff|wav|flac|m4a|ogg)$/,
+        exclude: /\.glyph.svg/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: { name: 'audio/[name].[ext]' },
+          },
+        ],
+      },
+    ],
+  },
+}
